1c1bc205411367a4d9c17f5a9a79fbc2328fe6ad
DefPro <cekurovaban@gmail.com>
2020-10-15 19:55:52 +0300

    First commit add all file

---
+++ js/script.js
@@ -0,0 +1,18 @@
+"use strict";
+
+// let number = 10;
+// const leftBorderWidth = 1;
+
+// number = 123;
+// console.log(number);
+
+// leftBorderWidth = 15;
+// console.log(leftBorderWidth);
+
+const answers = [];
+
+answers[0] = prompt('Как ваше имя?', '');
+answers[1] = prompt('Как ваша фамилия?', '');
+answers[2] = prompt('Сколько вам лет?', '');
+
+document.write(answers);
